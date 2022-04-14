5d05202cffa5631e796db21baf46e4b397338f1d
vinicus fraiz <viniciusfraiz10@gmail.com>
2022-04-14 20:06:39 -0300

    adicionando icone na aplicaçao

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
 });
 
+
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      icon:__dirname + "/imagem.png"
     })
   
     win.loadFile('index.html')
