0cbfc8f5a1a2b6cef427afe3d8c7594757e79889
VINICIUS FRAIZ <vinicius.fraiz@senacsp.edu.br>
2022-04-13 21:24:53 -0300

    firts commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
